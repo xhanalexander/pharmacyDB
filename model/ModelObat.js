66cb4de6f29eba1875c11209b3688e089fda62f7
xhanalexander <xhanalexander@gmail.com>
2022-12-27 18:54:10 +0700

    news

---
+++ model/ModelObat.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const {model, Schema} = require('mongoose');
+
+const ObatSchema = new Schema({
+   kode: {
+      type: String,
+      maxLength: [4,'kode obat tidak boleh lebih dari 4 karakter'],
+      required: [true, 'kode obat harus diisi']
+   },
+   nama: {
+      type: String,
+      required: [true, 'nama obat harus diisi']
+   },
+   kategori: {
+      type: String,
+      required: [true, 'golongan obat harus diisi']
+   },
+   jumlah: {
+      type: Number,
+      required: [true, 'jumlah obat harus diisi']
+   },
+   satuan: {
+      type: String,
+      required: [true, 'satuan obat harus diisi']
+   },
+   harga: {
+      type: Number,
+      required: [true, 'harga obat harus diisi']
+   },
+   produsen: {
+      type: String,
+      required: [true, 'produsen obat harus diisi']
+   },
+
+});
